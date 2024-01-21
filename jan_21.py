41f51a721735897898439a018ef72a897c64bcba
prarthana.m <prarthana.m@simformsolutions.com>
2024-01-21 11:04:47 +0530

    latest code

---
+++ jan_21.py
@@ -0,0 +1,125 @@
+
+# decision making: if-else-elif
+# a=24
+# b=23
+# if a==b:
+#     print("yes")
+#     if b >= 2:
+#         print(b)
+#     else:
+#         print("no")
+# # print("yes")
+# else:
+#     if b ==6:
+#         print("in else")
+#     elif b==2:
+#         print("---")
+#     elif b==7:
+#         print("===")
+#     else:
+#         print("no")
+
+# &&
+# a=2
+# b=5
+# if a == 2 and b == 7:
+#     print("1")
+# if a==2 or b==6:
+#     print("2")
+
+# l1=[1,2,3]
+# print(l1)
+# # for i in l1:
+# #     print(i)
+#     # print("hello") if i == 4 else print("hii")
+#     if name == 'pkm':
+#         print("3")
+#     if 5==3:
+#             print("5")
+#         elif 1==1:
+#             print("1")
+#         elif 6==2:
+#                 print("3")
+#         else:
+#             print("2")
+#     else:
+#         print("3")
+#     #     del l1[1]
+#     # else:
+#     #     print("hello")
+#
+#
+# print(l1)
+
+# take input as your name, check whether your name entered is correct or not
+# find greater number from two numbers
+# find maxnimum number between three elements
+# to check whether a person is eligible for voting
+# check whether person is senior citizen
+# write a program to check whether a number is positive or negative
+# write  program to check whether a number is odd or even
+
+
+# Write a Python program that take imput and check For multiples of three print "Fizz"
+# instead of the number and for multiples of five print "Buzz".
+# For numbers that are multiples of three and five, print "FizzBuzz".
+# Sample Output :
+# fizzbuzz
+# 1
+# 2
+# fizz
+# 4
+# buzz
+
+
+# Write a Python program to check the validity of passwords input by users.
+# Validation :
+
+# At least 1 character from [$#@].
+# Minimum length 6 characters.
+# Maximum length 16 characters.
+
+
+
+# Write a Python program to calculate a dog's age in dog years.
+# Note: For the first two years, a dog year
+# is equal to 10.5 human years. After that, each dog year equals 4 human years.
+# Expected Output:
+# Input a dog's age in human years: 15
+# The dog's age in dog's years is 73
+
+
+
+# Write a Python program to convert a month name to a number of days.
+# Expected Output:
+
+# List of months: January, February, March, April, May, June, July, August
+# , September, October, November, December
+# Input the name of Month: February
+# No. of days: 28/29 days
+
+
+
+
+# a=1
+# b=4
+# c=3
+# #
+# if a > b or b > c:
+#     print(a)
+
+# if "a">"sd":
+#     print("rdf")
+
+# a="india"
+# b="India"
+#
+# if a>b:
+#     print(a)
+# else:
+#     print(b)
+
+
+
+
+
