c59ba2d242114653c80479a3eedfdc65b4dbabac
prarthana.m <prarthana.m@simformsolutions.com>
2024-02-11 09:22:31 +0530

    latest code

---
+++ feb_4.py
@@ -0,0 +1,126 @@
+# looping:
+
+# for looping:
+
+# looping:
+# on string and list
+
+
+list1=[1,2,3,4,5,6,7,8,9,'1012345']
+# str1 = '12345678910'
+
+# for(i=0;i>10;i++)
+# for i in str1:
+#     print(i)
+
+# for i in list1:
+#     pass
+    # if i > 10:
+    #     print(i)
+
+
+
+# if complexity is 1
+# for complexcity is n
+
+# for i in str1:
+#     print(i)
+
+# for i in list1:
+#     print(i)
+
+
+str1='12345123456'
+# remove duplicates from this string
+
+
+
+
+# list1=[11,22,33,44,55,66,77,88,99,00]
+# sum of even and odd number from the list1
+
+
+# range()
+
+# list=[1,2,3,..10]
+
+# for i in range(24,25):
+    # print(i)
+
+
+# for i in range(len(list1)+1):
+#     print(list1[i])
+
+
+
+
+
+# take input from user and count sum of all odd number from the given range
+# list1=[1,2,3,4,5]
+# for i in range(len(list1)):
+#     # print(list1[i],i)
+#     if list1[i] % 2 == 0:
+#         print(list1[i])
+#     else:
+#         odd+=list1[i]
+
+
+# break, continue, pass
+
+
+
+
+
+
+# break:
+
+# for i in range(10):
+#     if i ==10:
+#         break
+#     print(i)
+
+# for i in range(1,10):
+   
+#     if i == 0:
+#         continue
+#     # else:
+#     print(i*i)
+    # print(i)
+
+
+# for i in "happy":
+#     # pass
+#     print(i)
+
+
+
+# remove duplicates
+# abc='1234556' 
+# list1=list(abc)
+# list2='12345'
+# for i in abc:
+#     if i not in list2:
+#         print(i)
+#         list2=list2+i
+#
+# print(''.join(list2))
+# print(list2)
+# print(str(list2))
+
+
+# Write a Python program that prints all the numbers except 3 and 6.
+# Note : Use 'continue' statement.
+# take range from user 
+# 50
+# 3,6,13,16,23,26,30,31..39,43,46
+# Expected Output : 0 1 2 4 5
+
+
+# Write a Python program to sum two integers.
+# However, if the sum is between 15 and 20 it will return 20. 
+
+
+# Write a Python program that checks whether a string represents an integer or not.
+# Expected Output:
+# Input a string: Python1
+# The string is  an integer.
